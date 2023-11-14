614b4036932e6bfc38d4c1bef10e57d0d50a2aa6
Jonatas <jonatasmateuscontato@gmail>
2023-11-14 14:06:33 -0300

    Make service works in the template

--- course/src/app/courses/courses.component.ts
+++ course/src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  portal: string;
+  courses: string[];
+
+  constructor(private cursosService: CoursesService) { 
+    
+    this.portal = 'http://loiane.training'
+    this.courses = this.cursosService.getCourses();
+
+  }
 }
--- course/src/app/courses/courses.service.ts
+++ course/src/app/courses/courses.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class CoursesService {
 
   constructor() { }
+
+  getCourses(){
+    return ['Java', 'Ext JS', 'Angular'];
+  }
 }
