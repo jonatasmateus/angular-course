e29e2804e642bbd808d15f169702e2c2507766ff
Jonatas <jonatasmateuscontato@gmail>
2023-11-18 16:48:18 -0300

    Work with Event Binding in courses component

--- course/src/app/courses/courses.component.ts
+++ course/src/app/courses/courses.component.ts
@@ -10,11 +10,36 @@ export class CoursesComponent {
 
   portal: string;
   courses: string[];
+  actualValue: string = '';  // To show the value received in #inputField
+  savedValue:string = '';  // To show the value entered or clicked outside from #inputField
+  isMouseOver: boolean = false;
 
   constructor(private cursosService: CoursesService) { 
     
     this.portal = 'http://loiane.training'
     this.courses = this.cursosService.getCourses();
 
+  }
+
+  clickedButton(){
+    alert('Clicked button!');
+  }
+
+	/* For intellisense to work with the event, use variable typing. */
+  onKeyUp(event: KeyboardEvent){
+		/* 
+			If we use `evento.target,value`, it gives a compilation error, because 
+      the `event.target` is of type <HTMLInputElement> . It is an interface
+      that TS uses to deal with JavaScript object types.
+		 */
+    this.actualValue = (<HTMLInputElement>event.target).value;
+  }
+
+  saveValue(value:string){
+    this.savedValue = value;
+  }
+
+	onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 }
