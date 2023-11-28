cd8ef0a7eaf091609f76d828b39af5a769a90247
Jonatas <jonatasmateuscontato@gmail>
2023-11-28 10:21:59 -0300

    Make functional the routing module
    In the app-routing.module, there are two paths configured: one to login page and one to courses page (like homepage).

--- course/src/app/app-routing.module.ts
+++ course/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesComponent } from './courses/courses.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: CoursesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
